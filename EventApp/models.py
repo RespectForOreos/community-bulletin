912be47fdbb57a07cfc6e85eb514e4cdcbc4e9a1
Hien Nguyen <82673040+RespectForOreos@users.noreply.github.com>
2024-01-03 21:34:56 -0800

    Delete EventApp directory

--- EventApp/models.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from app import db
-
-class Event(db.Model): 
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), unique = False, index = True)
-    description = db.Column(db.String(250), unique = False, index = True)
-    date = db.Column(db.String(40), unique = False, index = True)
-    time = db.Column(db.String(40), unique = False, index = True)
